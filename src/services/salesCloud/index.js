0e9e1b32f75faea5c1d9b166b55a5c753c1581da
fdxruli <48109415+fdxruli@users.noreply.github.com>
2026-06-25 05:34:33 -0600

    feat(sales-cloud): expose sales cloud module

---
+++ src/services/salesCloud/index.js
@@ -0,0 +1,5 @@
+export { salesCloudRepository } from './salesCloudRepository';
+export { salesCloudLocalRepository } from './salesCloudLocalRepository';
+export { salesCloudShadowService } from './salesCloudShadowService';
+export { salesCloudSyncHandler, registerSalesCloudSyncHandler } from './salesCloudSyncHandler';
+export { localSaleToCloudShadowPayload, cloudSaleToLocalSyncPatch } from './salesCloudMapper';
