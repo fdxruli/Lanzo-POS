d44b73f0b30f373a95fed4fc516a1cd795b5106b
fdxruli <48109415+fdxruli@users.noreply.github.com>
2026-06-25 13:19:47 -0600

    feat(sales-cloud): export cloud cashier sales services

--- src/services/salesCloud/index.js
+++ src/services/salesCloud/index.js
@@ -1,5 +1,12 @@
 export { salesCloudRepository } from './salesCloudRepository';
 export { salesCloudLocalRepository } from './salesCloudLocalRepository';
 export { salesCloudShadowService } from './salesCloudShadowService';
+export { salesCloudCashierService } from './salesCloudCashierService';
 export { salesCloudSyncHandler, registerSalesCloudSyncHandler } from './salesCloudSyncHandler';
 export { localSaleToCloudShadowPayload, cloudSaleToLocalSyncPatch } from './salesCloudMapper';
+export {
+  normalizeCloudCashierPaymentMethod,
+  isCreditLikePaymentMethod,
+  isCloudCashierCompatiblePayment,
+  mapLocalCheckoutToCloudSale
+} from './salesCloudCashierMapper';
