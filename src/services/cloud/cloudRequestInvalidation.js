2367d8742e6dd33d3939c70193f06000a3aead92
fdxruli <48109415+fdxruli@users.noreply.github.com>
2026-06-27 21:00:18 -0600

    Add cloud request invalidation helpers

---
+++ src/services/cloud/cloudRequestInvalidation.js
@@ -0,0 +1,51 @@
+import { CLOUD_REQUEST_TAGS } from './cloudRequestConstants';
+import { cloudRequestManager } from './cloudRequestManager';
+import { cloudRequestTags } from './cloudRequestKeys';
+
+const uniqueTags = (tags = []) => Array.from(new Set(tags.filter(Boolean)));
+
+export const invalidateCloudRequestTags = (tags = []) => uniqueTags(tags)
+  .reduce((count, tag) => count + cloudRequestManager.invalidateByTag(tag), 0);
+
+export const invalidateCloudCacheForLicense = (licenseKey) => {
+  if (!licenseKey) return 0;
+  return cloudRequestManager.invalidateByTag(cloudRequestTags.license(licenseKey));
+};
+
+export const invalidateCloudCacheAfterSaleMutation = (licenseKey) => invalidateCloudRequestTags([
+  cloudRequestTags.license(licenseKey),
+  CLOUD_REQUEST_TAGS.SALES,
+  CLOUD_REQUEST_TAGS.CASH,
+  CLOUD_REQUEST_TAGS.PRODUCTS,
+  CLOUD_REQUEST_TAGS.CUSTOMERS,
+  CLOUD_REQUEST_TAGS.CUSTOMER_CREDIT,
+  CLOUD_REQUEST_TAGS.REPORTS
+]);
+
+export const invalidateCloudCacheAfterCashMutation = (licenseKey) => invalidateCloudRequestTags([
+  cloudRequestTags.license(licenseKey),
+  CLOUD_REQUEST_TAGS.CASH,
+  CLOUD_REQUEST_TAGS.REPORTS
+]);
+
+export const invalidateCloudCacheAfterCatalogMutation = (licenseKey) => invalidateCloudRequestTags([
+  cloudRequestTags.license(licenseKey),
+  CLOUD_REQUEST_TAGS.PRODUCTS,
+  CLOUD_REQUEST_TAGS.SALES,
+  CLOUD_REQUEST_TAGS.REPORTS
+]);
+
+export const invalidateCloudCacheAfterCustomerMutation = (licenseKey) => invalidateCloudRequestTags([
+  cloudRequestTags.license(licenseKey),
+  CLOUD_REQUEST_TAGS.CUSTOMERS,
+  CLOUD_REQUEST_TAGS.CUSTOMER_CREDIT,
+  CLOUD_REQUEST_TAGS.REPORTS
+]);
+
+export const invalidateCloudCacheAfterCreditMutation = (licenseKey) => invalidateCloudRequestTags([
+  cloudRequestTags.license(licenseKey),
+  CLOUD_REQUEST_TAGS.CUSTOMER_CREDIT,
+  CLOUD_REQUEST_TAGS.CUSTOMERS,
+  CLOUD_REQUEST_TAGS.CASH,
+  CLOUD_REQUEST_TAGS.REPORTS
+]);
